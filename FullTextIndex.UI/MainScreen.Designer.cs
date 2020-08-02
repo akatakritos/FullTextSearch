85a7ea4a79f4179a1c48401bff8b8172061dc51c
Matt Burke <matt.burke@artisantechgroup.com>
2020-08-02 12:00:16 -0500

    Add UI for testing searches

---
+++ FullTextIndex.UI/MainScreen.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace FullTextIndex.UI
+{
+    partial class MainScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lvResults = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblSearchResults = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblSearchResults,
+            this.lblStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(59, 17);
+            this.lblStatus.Text = "Loading...";
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSearch.Location = new System.Drawing.Point(12, 12);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(695, 20);
+            this.txtSearch.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSearch.Enabled = false;
+            this.btnSearch.Location = new System.Drawing.Point(713, 10);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lvResults
+            // 
+            this.lvResults.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.lvResults.HideSelection = false;
+            this.lvResults.Location = new System.Drawing.Point(12, 41);
+            this.lvResults.Name = "lvResults";
+            this.lvResults.Size = new System.Drawing.Size(776, 384);
+            this.lvResults.TabIndex = 5;
+            this.lvResults.UseCompatibleStateImageBehavior = false;
+            this.lvResults.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Title";
+            this.columnHeader1.Width = 186;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Abstract";
+            this.columnHeader2.Width = 300;
+            // 
+            // lblSearchResults
+            // 
+            this.lblSearchResults.Name = "lblSearchResults";
+            this.lblSearchResults.Size = new System.Drawing.Size(695, 17);
+            this.lblSearchResults.Spring = true;
+            this.lblSearchResults.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // MainScreen
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvResults);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.statusStrip1);
+            this.Name = "MainScreen";
+            this.Text = "Full Text Search";
+            this.Load += new System.EventHandler(this.MainScreen_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblStatus;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.ListView lvResults;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ToolStripStatusLabel lblSearchResults;
+    }
+}
+
---
+++ FullTextIndex.UI/MainScreen.cs
@@ -0,0 +1,60 @@
+﻿using FullTextIndex.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FullTextIndex.UI
+{
+    public partial class MainScreen : Form
+    {
+        private InvertedIndex index;
+        public MainScreen()
+        {
+            InitializeComponent();
+        }
+
+        private async Task PopulateIndex()
+        {
+            await Task.Run(() =>
+            {
+                var entries = EntryReader.ReadDump(@"C:\Users\matt.burke.POINT\Downloads\enwiki-latest-abstract1.xml\enwiki-latest-abstract1.xml");
+
+                var invertedIndex = new InvertedIndex(700_000);
+                foreach (var entry in entries)
+                    invertedIndex.Index(entry);
+
+                this.index = invertedIndex;
+            });
+
+            lblStatus.Text = $"Index: {index.DocumentCount:n0} docs {index.TermCount:n0} terms";
+            btnSearch.Enabled = true;
+        }
+
+        private void MainScreen_Load(object sender, EventArgs e)
+        {
+            PopulateIndex();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            lvResults.Items.Clear();
+            lvResults.BeginUpdate();
+            var sw = Stopwatch.StartNew();
+            foreach(var entry in index.Search(txtSearch.Text))
+            {
+                lvResults.Items.Add(new ListViewItem(new string[] { entry.Title, entry.Abstract }));
+            }
+            sw.Stop();
+            lvResults.EndUpdate();
+            lvResults.Columns[1].Width = -2;
+            lblSearchResults.Text = $"Loaded {lvResults.Items.Count:n0} documents in {sw.ElapsedMilliseconds}ms";
+        }
+    }
+}
